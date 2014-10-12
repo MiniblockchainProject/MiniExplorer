bf8ad8c39f3bc8358bcf44d2f9151d5698ac98bf
MiniblockchainProject <MiniblockchainProject@users.noreply.github.com>
2014-10-13 07:29:02 +1100

    Added API page link to menu

--- inc/blocks/menu.inc.php
+++ inc/blocks/menu.inc.php
@@ -4,7 +4,7 @@
 				<li><a href="./">Home</a></li>
 				<li><a href="./?page=stats">Stats</a></li>
 				<li><a href="./?page=search">Search</a></li>
-				<!--<li><a href="./?page=api">API</a></li>-->
+				<li><a href="./?page=api">API</a></li>
 			</ul>
 			<form class="navbar-search pull-right right_gap" method="get" action="./index.php">
 				<input type="hidden" name="page" value="search" />
