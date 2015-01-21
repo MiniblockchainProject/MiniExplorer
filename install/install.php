32ff61588f1cefbad1e6cf0b600b967fcc052b48
MiniblockchainProject <MiniblockchainProject@users.noreply.github.com>
2015-01-21 17:20:04 +1100

    Added code for orphan blocks

--- install/install.php
+++ install/install.php
@@ -1,3 +1,12 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="UTF-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
+	<meta name="viewport" content="width=device-width, initial-scale=1.0" />
+	<title>Install MiniExplorer</title>
+</head>
+<body style="padding:20px;text-align:center;">
 <?php
 // create these files and folders in PHP to
 // ensure that PHP has necessary permissions
@@ -18,13 +27,31 @@ if (!file_exists('./../db/bhashes')) {
 	}
 }
 
+if (!file_exists('./../db/orph_dat')) {
+    if (file_put_contents('./../db/orph_dat', '0:0')) {
+	  chmod('./../db/orph_dat', 0700);
+	} else {
+	  die('error: unable to create /db/last_dat');
+	}
+}
+
+if (!file_exists('./../db/ohashes')) {
+    if (file_put_contents('./../db/ohashes', '') !== false) {
+	  chmod('./../db/ohashes', 0700);
+	} else {
+	  die('error: unable to create /db/ohashes');
+	}
+}
+
 if (!file_exists("./../db/txs/")) {
   if (!mkdir("./../db/txs/", 0700)) {
     die('error: unable to create /db/txs/');
   }
 }
 ?>
-
 <h1>Installation Successful</h1>
 
-<p>To finish up you should delete the install folder then create a cron job which runs /cron/parse_txs.php once per minute. It will take along time to complete the first time it runs so you should run the script once before creating the cron job. If you get the error 'could not get block/tx' and you're sure you have the full chain then try waiting a few seconds then start the script again.</p>
+<p>To finish up you should delete the install folder then edit the /inc/config.inc.php file and apply the correct settings. Next you will need to create a cron job which runs /cron/parse_txs.php once per minute. It will take a long time to complete the first time it runs so you should manually run the script once before creating the cron job. If you get the error 'could not get block/tx' and you're sure you have the full chain just wait a few seconds then start the script again (the daemon will temporarily stop working if overloaded). You will probably have to restart the script many times the first time it processes the full blockchain.</p>
+
+</body>
+</html>
