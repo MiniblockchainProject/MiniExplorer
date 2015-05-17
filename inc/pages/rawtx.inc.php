e80103518241dd44c21ac8f311eb501ce4fa0c4b
MiniblockchainProject <MiniblockchainProject@users.noreply.github.com>
2015-05-18 01:29:46 +1000

    Fixed json parsing error

--- inc/pages/rawtx.inc.php
+++ inc/pages/rawtx.inc.php
@@ -1,9 +1,11 @@
+<h1>Raw Transaction</h1>
+<br />
+
 <?php
 $tx_id = preg_replace("/[^a-f0-9]/", '', strtolower($_GET['rawtx']));
 $raw_tx = $_SESSION[$rpc_client]->getrawtransaction($tx_id, 1);
 
 if (rpc_error_check(false)) {
-  header('Content-Type: application/json');
-  echo json_encode($raw_tx);
+  echo '<pre>'.json_encode($raw_tx, JSON_PRETTY_PRINT).'</pre>';
 }
 ?>
