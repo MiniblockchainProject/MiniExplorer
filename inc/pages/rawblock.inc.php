aea01c1da5183e29b0ef3a4f146ee865f89f38b0
MiniblockchainProject <MiniblockchainProject@users.noreply.github.com>
2015-05-18 01:46:32 +1000

    Fixed json parsing error

--- inc/pages/rawblock.inc.php
+++ inc/pages/rawblock.inc.php
@@ -1,9 +1,11 @@
+<h1>Raw Block</h1>
+<br />
+
 <?php
 $block_id = preg_replace("/[^a-f0-9]/", '', strtolower($_GET['rawblock']));
 $raw_block = $_SESSION[$rpc_client]->getblock($block_id);
 
 if (rpc_error_check(false)) {
-  header('Content-Type: application/json');
-  echo json_encode($raw_block);
+  echo '<pre>'.json_encode($raw_block, JSON_PRETTY_PRINT).'</pre>';
 }
 ?>
